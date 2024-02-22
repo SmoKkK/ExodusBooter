b6c5875cc98511c6849f76974db0a9d9844af4bd
SmoKkK <kellishere7@gmail.com>
2024-02-22 12:19:20 +0200

    Main Release

---
+++ methods.php
@@ -0,0 +1,6 @@
+<?php
+// Format: "command" => "method_name" (possible args can be $time, $host, $port)
+$methods = [
+    "perl stdhex.pl $host $time" => "udphex",
+];
+?>
---
+++ servers.php
@@ -0,0 +1,8 @@
+<?php
+// AICI FUTUTI MORTI MOTI BAGI VPSURI, FORMATU E iP:UsER:PAROLA MS PWP
+$vpslist = [
+    //"1.2.3.4:nasapaul:bere",
+];
+
+$vpslist_spoofed = []; //WIP
+?>
